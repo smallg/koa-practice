893d0d80047d3639063ae86b722dd273aa797868
smallg <gwpxjb@163.com>
2023-05-22 23:09:07 +0800

    N/A: Add mysql and sequelize.

---
+++ src/db/seq.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize');
+const {
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PWD,
+  MYSQL_DB,
+} = require('../config/config.default');
+const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
+  host: MYSQL_HOST,
+  dialect: 'mysql',
+});
+
+try {
+  seq
+    .authenticate()
+    .then(() => {
+      console.log('success');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+} catch (err) {
+  console.log(err);
+}
+
+module.exports = seq;
